557cbbcd1e9c285961eae0c82075e4c420ca1825
zhangll <lzz.jason@gmail.com>
2013-11-15 07:19:44 +0800

    add ubuntu notify

---
+++ notify/putils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+
+    utils...
+
+'''
+
+class utils(object):
+
+    def __init__(self):
+        pass
+
+    def bulu(self,icon=None,title=None,content=None):
+        '''
+            pop bubble message with ubuntu notify-send
+
+        '''
+        import subprocess
+        return subprocess.call([ 'notify-send', '-i', icon, title, content]) 
+
+if __name__ == "__main__":
+    utils  =  utils()
+    utils.bulu('/tmp/27608.jpg','a','hahhah')
+
+
